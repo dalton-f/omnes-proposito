7d93ec19fc066fb9745fe5a587574cc1fb4d4a60
Dalton <daltonfiggdavies@gmail.com>
2025-07-11 08:30:46 +0100

    Upgrade ban command to use confirmation buttons

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -2,6 +2,9 @@ const {
   SlashCommandBuilder,
   MessageFlags,
   PermissionsBitField,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
 } = require("discord.js");
 
 module.exports = {
@@ -31,15 +34,56 @@ module.exports = {
       });
     }
 
-    const member = interaction.options.getMember("target");
+    const target = interaction.options.getMember("target");
     const reason =
-      interaction.options.getString("reason") || "No reason provided.";
+      interaction.options.getString("reason") ?? "No reason provided.";
 
-    interaction.guild.members.ban(member, { reason });
+    const confirm = new ButtonBuilder()
+      .setCustomId("confirm")
+      .setLabel("Confirm Ban")
+      .setStyle(ButtonStyle.Danger);
 
-    return interaction.reply({
-      content: `${member.user.tag} has been banned from the server for: ${reason}`,
+    const cancel = new ButtonBuilder()
+      .setCustomId("cancel")
+      .setLabel("Cancel")
+      .setStyle(ButtonStyle.Secondary);
+
+    const row = new ActionRowBuilder().addComponents(confirm, cancel);
+
+    const response = await interaction.reply({
+      content: `Are you sure you want to ban ${target} for reason: ${reason}?`,
+      components: [row],
+      withResponse: true,
       flags: MessageFlags.Ephemeral,
     });
+
+    const collectorFilter = (i) => i.user.id === interaction.user.id;
+
+    try {
+      const confirmation =
+        await response.resource.message.awaitMessageComponent({
+          filter: collectorFilter,
+          time: 60_000,
+        });
+
+      if (confirmation.customId === "confirm") {
+        await interaction.guild.members.ban(target, { reason });
+
+        await confirmation.update({
+          content: `${target} has been banned for reason: ${reason}`,
+          components: [],
+        });
+      } else if (confirmation.customId === "cancel") {
+        await confirmation.update({
+          content: "Action cancelled",
+          components: [],
+        });
+      }
+    } catch {
+      await interaction.editReply({
+        content: "Confirmation not received within 1 minute, cancelling",
+        components: [],
+      });
+    }
   },
 };
